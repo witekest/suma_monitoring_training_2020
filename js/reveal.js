ccfc1059fc9be31547c3bc16ea3b0e6bf6943eee
Adam Spiers <github@adamspiers.org>
2019-04-02 15:14:11 -0500

    add link to dependencies

--- js/reveal.js
+++ js/reveal.js
@@ -1,5 +1,6 @@
-// Full list of configuration options available here:
-// https://github.com/hakimel/reveal.js#configuration
+// More info about config & dependencies:
+// - https://github.com/hakimel/reveal.js#configuration
+// - https://github.com/hakimel/reveal.js#dependencies
 Reveal.initialize({
     // If you change these, make sure $aspect-ratio in the .scss file
     // is updated accordingly:
